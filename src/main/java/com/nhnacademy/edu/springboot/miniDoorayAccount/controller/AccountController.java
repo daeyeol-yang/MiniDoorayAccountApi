fcb2a22947d1625301b754af26ae9de8d447adc8
Daeyoul-Yang <cnrrnehfdl03@naver.com>
2024-01-26 15:19:28 +0900

    modify signup enum

--- src/main/java/com/nhnacademy/edu/springboot/miniDoorayAccount/controller/AccountController.java
+++ src/main/java/com/nhnacademy/edu/springboot/miniDoorayAccount/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.nhnacademy.edu.springboot.miniDoorayAccount.controller;
 import com.nhnacademy.edu.springboot.miniDoorayAccount.entity.Account;
 import com.nhnacademy.edu.springboot.miniDoorayAccount.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,18 +11,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
 
-    @PostMapping("/accounts")
+    @PostMapping("/signup")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<String> createAccount(@RequestBody Account account) {
        try {
            accountService.createAccount(account);
            return ResponseEntity.status(HttpStatus.CREATED).body("Success");
        }catch (RuntimeException e){
+           log.info(e.getMessage());
            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
        }
 
--- src/main/java/com/nhnacademy/edu/springboot/miniDoorayAccount/controller/LoginController.java
+++ src/main/java/com/nhnacademy/edu/springboot/miniDoorayAccount/controller/LoginController.java
@@ -6,6 +6,7 @@ import com.nhnacademy.edu.springboot.miniDoorayAccount.service.AccountService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class LoginController {
@@ -21,6 +23,7 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<Account> doLogin(@RequestBody LoginRequest loginRequest) {
+        log.info("Login -----");
         if (accountService.matches(loginRequest.getId(), loginRequest.getPwd())) {
             return ResponseEntity.status(HttpStatus.OK).body(accountService.getAccount(loginRequest.getId()));
         }
